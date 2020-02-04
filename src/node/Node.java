d62d722fbee76d5067980eef3d4ab953354341a3
HeshanSudarshana <heshan.15@cse.mrt.ac.lk>
2020-02-04 15:14:24 +0530

    nodes updated

--- src/node/Node.java
+++ src/node/Node.java
@@ -6,10 +6,36 @@ public class Node {
     private BootstrapServer bootstrapServer;
     private String nodeName;
     private String ip;
-    private String port;
+    private String sendPort;
+    private String recvPort;
     private ArrayList<Node> neighbours;
     private ArrayList<String> files;
 
+    public Node(BootstrapServer bootstrapServer, String nodeName, String ip, String sendPort, String recvPort, ArrayList<Node> neighbours, ArrayList<String> files) {
+        this.bootstrapServer = bootstrapServer;
+        this.nodeName = nodeName;
+        this.ip = ip;
+        this.sendPort = sendPort;
+        this.recvPort = recvPort;
+        this.neighbours = neighbours;
+        this.files = files;
+    }
+
+    public String getSendPort() {
+        return sendPort;
+    }
+
+    public void setSendPort(String sendPort) {
+        this.sendPort = sendPort;
+    }
+
+    public String getRecvPort() {
+        return recvPort;
+    }
+
+    public void setRecvPort(String recvPort) {
+        this.recvPort = recvPort;
+    }
 
     public BootstrapServer getBootstrapServer() {
         return bootstrapServer;
@@ -33,14 +59,6 @@ public class Node {
 
     public void setIp(String ip) {
         this.ip = ip;
-    }
-
-    public String getPort() {
-        return port;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
     }
 
     public ArrayList<Node> getNeighbours() {
