97417994d71862144ee5fc2bfea74f1cc76a0a9b
HeshanSudarshana <heshan.15@cse.mrt.ac.lk>
2020-02-04 22:28:31 +0530

    searchReq init

--- src/request/SearchReq.java
+++ src/request/SearchReq.java
@@ -6,5 +6,24 @@ public class SearchReq {
     private String fileName;
     private NodeData nodeData;
 
+    public SearchReq(String fileName, NodeData nodeData) {
+        this.fileName = fileName;
+        this.nodeData = nodeData;
+    }
 
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public NodeData getNodeData() {
+        return nodeData;
+    }
+
+    public void setNodeData(NodeData nodeData) {
+        this.nodeData = nodeData;
+    }
 }
